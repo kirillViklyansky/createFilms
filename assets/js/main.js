9bf2de142c3df9b5e4f086c6d9975310cc003db9
Kirill <keryauser@gmail.com>
2021-08-04 22:41:12 +0300

    fix li info, new bug with edit films

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const formPoster = document.getElementById('poster');
     const formDescription = document.getElementById('description');
     
-    const clean =  addNewFilmForm.reset = (e) =>{
+    const clean = addNewFilmForm.reset = (e) =>{
         formTitle.value = '';
         formYear.value = '';
         formCountry.value = '';
@@ -48,17 +48,36 @@ document.addEventListener('DOMContentLoaded', () => {
         mainFilmContentButtonEdit.className = 'main-film-content-button-edit';
         const ul = document.createElement('ul');
         
-        for (let i = 0; i <= 4; i++){
+        for (let i = 0; i <= 3; i++){
             const li = document.createElement('li');
             const mainFilmContentInfoDiv = document.createElement('div');
             mainFilmContentInfoDiv.className = 'main-film-content-info-div';
             const mainFilmContentInfoDivSpan = document.createElement('span');
             mainFilmContentInfoDivSpan.className = 'main-film-content-info-div_span';
-            mainFilmContentInfoDiv.id = Math.random()*10 + i;
+            mainFilmContentInfoDiv.id = '_' + i;
+            if(mainFilmContentInfoDiv.id == '_0'){
+                mainFilmContentInfoDiv.textContent = 'COUNRTY:';
+                mainFilmContentInfoDivSpan.textContent = formCountry.value;
+                mainFilmContentInfoDiv.id = Math.random()*10 + i;
+            }else if (mainFilmContentInfoDiv.id == '_1'){
+                mainFilmContentInfoDiv.textContent = 'Year:';
+                mainFilmContentInfoDivSpan.textContent = formYear.value;
+                mainFilmContentInfoDiv.id = Math.random()*10 + i;
+            }else if(mainFilmContentInfoDiv.id == '_2'){
+                mainFilmContentInfoDiv.textContent = 'GENER:';
+                mainFilmContentInfoDivSpan.textContent = formGener.value;
+                mainFilmContentInfoDiv.id = Math.random()*10 + i;
+            }else if(mainFilmContentInfoDiv.id == '_3'){
+                mainFilmContentInfoDiv.textContent = 'ACTORS:';
+                mainFilmContentInfoDivSpan.textContent = formPoster.value;
+                mainFilmContentInfoDiv.id = Math.random()*10 + i;
+            }
             ul.append(li);
             li.append(mainFilmContentInfoDiv);
             mainFilmContentInfoDiv.append(mainFilmContentInfoDivSpan);
         }
+        mainFilmContentH2.textContent = formTitle.value;
+        mainFilmContentText.textContent = formDescription.value;
 
         div.append(mainFilmBlock);
         mainFilmBlock.append(mainFilmImg);
